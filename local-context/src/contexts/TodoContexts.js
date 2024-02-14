2be25a35684b76c2c71cb66cc4b6ea916480c023
Shubham Bartwal <57596569+shubhambartwal@users.noreply.github.com>
2024-02-14 09:44:08 +0000

    todoAdvance app (local storage,contextApi)

--- local-context/src/contexts/TodoContexts.js
+++ local-context/src/contexts/TodoContexts.js
@@ -8,7 +8,7 @@ export const TodoContext = createContext({
     addTodo: (todo) => { },
     updateTodo: (id, todo) => { },
     deleteTodo: (id) => { },
-    toggleComplete: () => { }
+    toggleComplete: (id) => { }
 });
 export const useTodo = () => {
     return useContext(TodoContext)
